8a774e230e96d57b6ac5f58f85608b41a69644e7
Romi Petrelis <romipetrelis+github@gmail.com>
2023-10-26 12:59:15 -0500

    remove Bootstrap from interface

--- src/Domain.Abstractions/Infrastructure/IEventStore.cs
+++ src/Domain.Abstractions/Infrastructure/IEventStore.cs
@@ -10,6 +10,4 @@ public interface IEventStore
     Task<int> Save(IEvent @event, string aggregateId, string aggregateType, int? aggregateVersion=null);
     Task<IDictionary<int, IEvent>> GetEventsBy(string aggregateId);
     Task<int?> GetCurrentVersionNumberOf(string aggregateId);
-
-    Task Bootstrap();
 }
